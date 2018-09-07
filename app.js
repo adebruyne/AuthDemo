3c906ca100b87e883be4cbab209c80c9c5df171b
Aylin DeBruyne <adebruyne88@gmail.com>
2018-09-06 20:55:53 -0400

    add first route to home and views for home

---
+++ app.js
@@ -0,0 +1,14 @@
+var express =require('express');
+
+var app=express();
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+    res.render("home");
+});
+
+app.listen(8889, () => {
+    console.log("The  server has started!");
+  });
