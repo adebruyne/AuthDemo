14fd9abb462638d1cbbcbae258c441f644889094
Aylin DeBruyne <adebruyne88@gmail.com>
2018-09-06 21:10:18 -0400

    adding express-sessions

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ var passport = require("passport");
 var bodyParser = require("body-parser");
 var LocalStrategy = require("passport-local");
 var passportLocalMongoose = require("passport-local-mongoose");
+var User = require("./models/user");
 
 mongoose.connect("mongodb://localhost/auth_demo_app");
+
+
 
 var app = express();
 
 app.set("view engine", "ejs");
+app.use(
+    require("express-session")({
+      secret: "Rusty is the best and cutest dog in the world",
+      resave: false,
+      saveUninitialized: false
+    })
+  );
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", function(req, res) {
   res.render("home");
