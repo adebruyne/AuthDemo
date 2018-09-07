1b66e71ab180bd4a805dfbec9e74b299f3cdefcd
Aylin DeBruyne <adebruyne88@gmail.com>
2018-09-06 21:04:39 -0400

    add UserSchema

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
-var express =require('express');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var bodyParser = require('body-passport');
-var LocalStrategy = require('passport-local');
-var passportLocalMongoose = require('passport-local-mongoose');
+var express = require("express");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var bodyParser = require("body-parser");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://localhost/auth_demo_app");l
+mongoose.connect("mongodb://localhost/auth_demo_app");
 
+var app = express();
 
-var app=express();
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs');
-
-
-app.get('/', function(req, res){
-    res.render("home");
+app.get("/", function(req, res) {
+  res.render("home");
 });
 
-
-app.get('/secret', function(req, res){
-    res.render('secret');
-})
+app.get("/secret", function(req, res) {
+  res.render("secret");
+});
 
 app.listen(8889, () => {
-    console.log("The  server has started!");
-  });
+  console.log("The  server has started!");
+});
